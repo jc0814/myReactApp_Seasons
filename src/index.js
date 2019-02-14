b88e1bd961c904c6b423452487105cbcd8688868
jc0814 <0814.jessie@gmail.com>
2019-02-14 17:11:02 +0800

    02142019_state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'; 
+import ReactDOM from'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component{
+    //constructor function is a js function, auto have props 
+    constructor(props){
+        //super is a reference the the parent's constructor function.
+        super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        this.state = { lat: null };
+
+        //place the window.navigator to save the time, cuz it runs every time at the render()
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                //we called setState!!!
+                this.setState({ lat:position.coords.latitude});
+
+                //we did not
+                //this.state.lat = lat:position.coords.latitude;
+            },
+            (err)=> console.log(err) //error call back 
+        ); 
+    }
+
+    //React says we have to define rendre !!
+    render(){
+        
+
+        return <div>Latitude: {this.state.lat} </div>
+    }
+}
+ReactDOM.render(<App/>, document.querySelector('#root'));
